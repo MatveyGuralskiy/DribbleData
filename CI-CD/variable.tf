633c0a67106453106fac3688389a70a319f63596
MatveyGuralskiy <mathewguralskiy@gmail.com>
2024-08-07 10:14:03 +0300

    Create SSM Python script

--- CI-CD/variable.tf
+++
@@ -1,9 +0,0 @@
-variable "Region" {
-  description = "Default Region to work with"
-  type        = string
-  default     = "us-east-1"
-}
-
-variable "Project_name" {
-  default = "CodeBuild-Test"
-}
