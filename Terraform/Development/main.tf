f98692276bb1074bdada968de7157270199a3a24
MatveyGuralskiy <mathewguralskiy@gmail.com>
2024-08-11 20:22:21 +0300

    Update new version

--- Terraform/Development/main.tf
+++ Terraform/Development/main.tf
@@ -74,3 +74,34 @@ resource "aws_acm_certificate_validation" "DribbleData_cert_validation" {
   certificate_arn         = aws_acm_certificate.DribbleData_cert.arn
   validation_record_fqdns = [for record in aws_route53_record.DribbleData_cert_validation : record.fqdn]
 }
+
+# Create ACM Request
+resource "aws_acm_certificate" "DribbleData_cert_Prometheus" {
+  domain_name       = "prometheus.matveyguralskiy.com"
+  validation_method = "DNS"
+}
+
+# Create DNS Record in Route53
+resource "aws_route53_record" "DribbleData_cert_validation_Prometheus" {
+  # Dynamic Record
+  for_each = {
+    # Domain Validation Option for ACM
+    for dvo in aws_acm_certificate.DribbleData_cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      type   = dvo.resource_record_type
+      record = dvo.resource_record_value
+    }
+  }
+
+  zone_id = data.aws_route53_zone.Application_Zone.id
+  name    = each.value.name
+  type    = each.value.type
+  ttl     = 60
+  records = [each.value.record]
+}
+
+# DNS Validation of ACM
+resource "aws_acm_certificate_validation" "DribbleData_cert_validation_Prometheus" {
+  certificate_arn         = aws_acm_certificate.DribbleData_cert_Prometheus.arn
+  validation_record_fqdns = [for record in aws_route53_record.DribbleData_cert_validation_Prometheus : record.fqdn]
+}
